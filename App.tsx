67afec682f4fe26bac6ae9cf10dfb3168a3b90e8
elder-tome <r14.elderalves@gmail.com>
2025-01-31 16:16:09 -0300

    feat: configurando expo-update

--- App.tsx
+++ App.tsx
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Updates from "expo-updates";
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [updating, setUpdating] = useState(false);
+
+  const checkForUpdates = async () => {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      
+      if (update.isAvailable) {
+        setUpdating(true);
+
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+
+        setUpdating(false);
+      }
+    } catch (error) {
+      console.error("Erro ao buscar atualizações:", error);
+    }
+  };
+  
+  useEffect(() => {
+    checkForUpdates();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {
+        updating && <Text>Atualizando...</Text>
+      }
+      <Text>Versão 1.0.9</Text>
       <StatusBar style="auto" />
     </View>
   );
